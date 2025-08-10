21de34fa1682f2204157f50a80f932d96773245b
Benjamin Schmidt <92173565+BenAhrdt@users.noreply.github.com>
2025-08-10 11:00:26 +0200

    Create Trigger.js

---
+++ ioBroker/Skripte/Vicki/Aktionen vor Uplink/Trigger.js
@@ -0,0 +1,36 @@
+
+// Deklaration der enums, auf die Reagiert werden soll
+const EnumRegelgruppen = "ControlGroups";
+const EnumTemperaturTrigger = "Temperature.Trigger";
+const Vorzug = 10 * 1000;
+const MinIntervall = 1 * 60 * 1000;
+
+$(`state(${EnumRegelgruppen}=${EnumTemperaturTrigger})`).on((obj)=>{
+    const RoomId = getRoomId(obj.id);
+    const Intervall = obj.state.ts - obj.oldState.ts;
+    if(Intervall >= MinIntervall){
+        //log(`In ca. ${Math.round((Intervall - Vorzug)/1000)}s werden die Aktionen im Raum ${RoomId} ausgeführt`);
+        const delay = Math.max(0, Intervall - Vorzug); // nie negativ
+        const Data = {'RoomId':RoomId};
+        // setTimeout(fn, delay, ...args) → ...args werden an fn übergeben
+        // hier: callMessage(payload) nach Ablauf von "delay"
+        setTimeout(callMessage,delay,Data);
+    }
+});
+
+// Raum finden
+function getRoomId(id){
+    const myObj = getObject(id,"rooms");
+    // Raum vorhanden?
+    if(myObj.enumIds[0]){
+        return myObj.enumIds[0].substring("enum.rooms.".length,myObj.enumIds[0].length);
+    }
+    else{
+        return null;
+    }
+};
+
+function callMessage(Data){
+    messageTo('ThermostatActionBeforeUplink',Data);
+}
+
