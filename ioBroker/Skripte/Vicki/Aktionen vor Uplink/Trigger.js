18dae2ee3eb731715ebc3a3e28ec53336320f287
Benjamin Schmidt <92173565+BenAhrdt@users.noreply.github.com>
2025-10-06 13:52:36 +0200

    Update Trigger.js

--- ioBroker/Skripte/Vicki/Aktionen vor Uplink/Trigger.js
+++ ioBroker/Skripte/Vicki/Aktionen vor Uplink/Trigger.js
@@ -2,16 +2,21 @@
 // Deklaration der enums, auf die Reagiert werden soll
 const EnumRegelgruppen = "ControlGroups";
 const EnumTemperaturTrigger = "Temperature.Trigger";
-const Vorzug = 10 * 1000;
-const MinIntervall = 1 * 60 * 1000;
+const Vorzug = 30 * 1000;
+const MinIntervall = 3 * 60 * 1000;
+let LastTimestamp = {};
 
 $(`state(${EnumRegelgruppen}=${EnumTemperaturTrigger})`).on((obj)=>{
+    if (!LastTimestamp[obj.id]) {
+        LastTimestamp[obj.id] = obj.oldState.ts;
+    }
     const RoomId = getRoomId(obj.id);
-    const Intervall = obj.state.ts - obj.oldState.ts;
+    const Intervall = obj.state.ts - LastTimestamp[obj.id];
     if(Intervall >= MinIntervall){
+        LastTimestamp[obj.id] = obj.state.ts;
         //log(`In ca. ${Math.round((Intervall - Vorzug)/1000)}s werden die Aktionen im Raum ${RoomId} ausgeführt`);
         const delay = Math.max(0, Intervall - Vorzug); // nie negativ
-        const Data = {'RoomId':RoomId};
+        const Data = {'Intervall': Intervall, 'Vorzug': Vorzug, 'RoomId':RoomId, 'TriggerId': obj.id};
         // setTimeout(fn, delay, ...args) → ...args werden an fn übergeben
         // hier: callMessage(payload) nach Ablauf von "delay"
         setTimeout(callMessage,delay,Data);
