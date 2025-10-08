81b5000dbcb7bc0f6fe2af3750c60a50e0af8d51
Benjamin Schmidt <92173565+BenAhrdt@users.noreply.github.com>
2025-10-08 09:10:13 +0200

    Update Trigger.js

--- ioBroker/Skripte/Vicki/Aktionen vor Uplink/Trigger.js
+++ ioBroker/Skripte/Vicki/Aktionen vor Uplink/Trigger.js
@@ -4,16 +4,14 @@ const EnumRegelgruppen = "ControlGroups";
 const EnumTemperaturTrigger = "Temperature.Trigger";
 const Vorzug = 20 * 1000;
 const MinIntervall = 1 * 30 * 1000;
-let LastTimestamp = {};
 
 $(`state(${EnumRegelgruppen}=${EnumTemperaturTrigger})`).on((obj)=>{
-    if (!LastTimestamp[obj.id]) {
+/*    if (!LastTimestamp[obj.id]) {
         LastTimestamp[obj.id] = obj.oldState.ts;
-    }
+    }*/
     const RoomId = getRoomId(obj.id);
-    const Intervall = obj.state.ts - LastTimestamp[obj.id];
+    const Intervall = obj.state.ts - obj.oldState.ts;//LastTimestamp[obj.id];
     if(Intervall >= MinIntervall){
-        LastTimestamp[obj.id] = obj.state.ts;
         //log(`In ca. ${Math.round((Intervall - Vorzug)/1000)}s werden die Aktionen im Raum ${RoomId} ausgeführt`);
         const delay = Math.max(0, Intervall - Vorzug); // nie negativ
         const Data = {'Intervall': Intervall, 'Vorzug': Vorzug, 'RoomId':RoomId, 'TriggerId': obj.id};
@@ -38,4 +36,15 @@ function getRoomId(id){
 function callMessage(Data){
     messageTo('ThermostatActionBeforeUplink',Data);
 }
+
+
+on('alias.0.Talstrasse1A.RikasKinderzimmer.Thermostat.time',()=>{
+    log('time');
+})
+
+
+on('lorawan.1.bbea74d6-1fc5-4238-af20-d2aecdbb4f8e.devices.70b3d52dd301b3cc.uplink.raw.json',()=>{
+    log('json');
+})
+
 
